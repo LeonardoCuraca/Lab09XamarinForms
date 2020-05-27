783448f2c561ed0b677919dcb762b5401f1d6ca0
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-05-26 20:10:57 -0500

    first commit

---
+++ LabGesture/LabGesture/Models/HomeMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabGesture.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About,
+        TapDemo,
+        PinchDemo,
+        PanDemo,
+        SwipeDemo,
+        SwipeBinding
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ LabGesture/LabGesture/Views/MenuPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using LabGesture.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LabGesture.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MenuPage : ContentPage
+    {
+        MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+        List<HomeMenuItem> menuItems;
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            menuItems = new List<HomeMenuItem>
+            {
+                new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
+                new HomeMenuItem {Id = MenuItemType.About, Title="About" },
+                new HomeMenuItem {Id = MenuItemType.TapDemo, Title="TAP" },
+                new HomeMenuItem {Id = MenuItemType.PinchDemo, Title="PINCH" },
+                new HomeMenuItem {Id = MenuItemType.PanDemo, Title="PAN" },
+                new HomeMenuItem {Id = MenuItemType.SwipeDemo, Title="SWIPE"},
+                new HomeMenuItem {Id = MenuItemType.SwipeBinding, Title="SWIPEBINDING"}
+            };
+
+            ListViewMenu.ItemsSource = menuItems;
+
+            ListViewMenu.SelectedItem = menuItems[0];
+            ListViewMenu.ItemSelected += async (sender, e) =>
+            {
+                if (e.SelectedItem == null)
+                    return;
+
+                var id = (int)((HomeMenuItem)e.SelectedItem).Id;
+                await RootPage.NavigateFromMenu(id);
+            };
+        }
+    }
+}
